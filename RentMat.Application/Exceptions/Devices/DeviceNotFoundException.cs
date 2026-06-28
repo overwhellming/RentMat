9ee08574a518de6ff2980577a45208ea2ec71749
rizzn <nagimovvazir@gmail.com>
2026-06-28 19:11:14 +0500

    IExceptionBase for better exception hadling with IProblemDetailsService
    (cherry picked from commit 2172a494b972178efe2f98a71315c2c74c1175ab)

--- RentMat.Application/Exceptions/Devices/DeviceNotFoundException.cs
+++ RentMat.Application/Exceptions/Devices/DeviceNotFoundException.cs
@@ -1,3 +1,7 @@
 namespace RentMat.Application.Exceptions.Devices;
 
-public class DeviceNotFoundException(int id) : Exception($"Device with id {id} was not found");
+public class DeviceNotFoundException (int id) : Exception($"Device with id {id} was not found"), IExceptionBase
+{
+    public HttpStatusCode StatusCode => HttpStatusCode.NotFound;
+    public string Title => "Device was not found";
+}
--- RentMat.Application/Exceptions/Users/NotEnoughMoneyException.cs
+++ RentMat.Application/Exceptions/Users/NotEnoughMoneyException.cs
@@ -1,3 +1,9 @@
+using System.Net;
+
 namespace RentMat.Application.Exceptions.Users;
 
-public class NotEnoughMoneyException(int id) : Exception($"User with id {id} doesn't have enough money");
+public class NotEnoughMoneyException (int id) : Exception($"User with id {id} doesn't have enough money"), IExceptionBase
+{
+    public HttpStatusCode StatusCode => HttpStatusCode.Conflict;
+    public string Title => "User doesn't have enough money";
+}
